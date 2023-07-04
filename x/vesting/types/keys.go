677516da6315816192077893797d075d8723007a
DESKTOP-33MSE7M\admin <790228724@qq.com>
2023-07-04 14:21:28 +0800

    no message

---
+++ x/vesting/types/keys.go
@@ -0,0 +1,15 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package types
+
+const (
+	// ModuleName defines the module's name.
+	ModuleName = "vesting"
+
+	// StoreKey to be used when creating the KVStore
+	StoreKey = ModuleName
+
+	// RouterKey defines the module's message routing key
+	RouterKey = ModuleName
+)
